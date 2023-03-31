aca6b69f44c0668267efd4179a9b556187bbae6d
Le Duy Nguyen <xnguye27@stud.fit.vut.cz>
2023-04-01 01:22:20 +0200

    Update `Number` inner data structure

--- src/math/src/number.rs
+++ src/math/src/number.rs
@@ -1,15 +1,21 @@
 /// Represent a number
-pub struct Number(f64);
+pub struct Number {
+    inner: f64,
+    precision: u8,
+}
 
 impl AsRef<f64> for Number {
     fn as_ref(&self) -> &f64 {
-        &self.0
+        &self.inner
     }
 }
 
 impl Number {
     /// 0.0
-    pub const ZERO: Number = Self(0.0);
+    pub const ZERO: Self = Self {
+        inner: 0.0,
+        precision: 6,
+    };
 
     ///
     pub fn to_string_radix(&self, _radix: u8) -> String {
