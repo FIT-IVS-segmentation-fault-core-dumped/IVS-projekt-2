6d1314858a30dcec8c7b92ec38531261d30e73d2
Le Duy Nguyen <xnguye27@stud.fit.vut.cz>
2023-04-01 01:24:14 +0200

    Add `to_string` function family to `Number`

--- src/math/src/number.rs
+++ src/math/src/number.rs
@@ -29,8 +29,77 @@ impl Number {
         precision: 6,
     };
 
+    /// Set the maximum precision of the Number
+    /// This only affect the `to_string` (and its related) function
+    /// The calculation is unaffected
     ///
-    pub fn to_string_radix(&self, _radix: u8) -> String {
+    /// ```
+    /// let mut e = Number::E;
+    /// assert_eq!(e.to_string(), "2.718281");
+    /// e.set_precision(2);
+    /// assert_eq!(e.to_string(), "2.71");
+    /// ```
+    pub fn set_precision(&mut self, precision: u8) {
+        self.precision = precision;
+    }
+
+    /// Get the decimal string
+    ///
+    /// ```
+    /// let pi = Number::PI.to_string();
+    /// let zero = Number::ZERO.to_string();
+    /// let neg = Number::from(-0.1).to_string();
+    ///
+    /// assert_eq!(zero, "0");
+    /// assert_eq!(neg, "-0.1");
+    /// assert!(pi, "3.141592"); // default precision is 6 decimal points
+    /// ```
+    pub fn to_string(&self) -> String {
+        todo!()
+    }
+
+    /// Get the binary string
+    ///
+    /// ```
+    /// let pi = Number::PI.to_string_binary();
+    /// let zero = Number::ZERO.to_string_binary();
+    /// let neg = Number::from(-0.1).to_string_binary();
+    ///
+    /// assert_eq!(zero, "0");
+    /// assert_eq!(neg, "-0.1");
+    /// assert!(pi.starts_with("3.14159"));
+    /// ```
+    pub fn to_string_binary(&self) -> String {
+        todo!()
+    }
+
+    /// Get the octal string
+    ///
+    /// ```
+    /// let pi = Number::PI.to_string_octal();
+    /// let zero = Number::ZERO.to_string_octal();
+    /// let neg = Number::from(-0.1).to_string_octal();
+    ///
+    /// assert_eq!(zero, "0");
+    /// assert_eq!(neg, "-0.00011");
+    /// assert!(pi, "11.001001");
+    /// ```
+    pub fn to_string_octal(&self) -> String {
+        todo!()
+    }
+
+    /// Get the hexadecimal string
+    ///
+    /// ```
+    /// let pi = Number::PI.to_string_hex();
+    /// let zero = Number::ZERO.to_string_hex();
+    /// let neg = Number::from(-0.1).to_string_hex();
+    ///
+    /// assert_eq!(zero, "0");
+    /// assert_eq!(neg, "-0.199999");
+    /// assert!(pi.starts_with("3.243F6A"));
+    /// ```
+    pub fn to_string_hex(&self) -> String {
         todo!()
     }
 }
