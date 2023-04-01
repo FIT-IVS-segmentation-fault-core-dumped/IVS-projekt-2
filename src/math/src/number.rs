7fe94cdc548e83dcf1b6f27e314a946a40909d9c
Le Duy Nguyen <xnguye27@stud.fit.vut.cz>
2023-04-01 23:28:56 +0200

    Change the API of formatting a Number into a String

--- src/math/src/number.rs
+++ src/math/src/number.rs
@@ -1,10 +1,25 @@
 use crate::Result;
 use std::cmp::Ordering;
 
-/// Represent a number
 #[derive(Debug, Clone, Copy)]
+/// Represent a number
 pub struct Number {
     inner: f64,
+}
+
+#[derive(Default, Debug, Clone, Copy)]
+#[non_exhaustive]
+/// Radix to use to represent a Number
+pub enum Radix {
+    /// Binary
+    Bin,
+    /// Octal
+    Oct,
+    #[default]
+    /// Decimal - default
+    Dec,
+    /// Hexadecimal
+    Hex,
 }
 
 impl<T: Into<f64>> From<T> for Number {
@@ -18,6 +33,7 @@ impl AsRef<f64> for Number {
         &self.inner
     }
 }
+
 impl Number {
     /// 0.0
     pub const ZERO: Self = Self { inner: 0.0 };
@@ -35,63 +51,32 @@ impl Number {
         inner: std::f64::consts::E,
     };
 
-    /// Get the decimal string
+    /// Get the formatted string of a number
     ///
     /// ```
-    /// let pi = Number::PI.to_string(6);
-    /// let zero = Number::ZERO.to_string(6);
-    /// let neg = Number::from(-0.1).to_string(6);
+    /// # use math::number::{Number, Radix};
+    /// let pi = Numper::PI;
+    /// let zero = Number::ZERO;
+    /// let neg = Number::from(-0.1);
     ///
-    /// assert_eq!(zero, "0");
-    /// assert_eq!(neg, "-0.1");
-    /// assert!(pi, "3.141592"); // default precision is 6 decimal points
-    /// ```
-    pub fn to_string(&self, precision: u8) -> String {
-        todo!()
-    }
-
-    /// Get the binary string
+    /// let precision = 6;
     ///
-    /// ```
-    /// let pi = Number::PI.to_string_binary(6);
-    /// let zero = Number::ZERO.to_string_binary(6);
-    /// let neg = Number::from(-0.1).to_string_binary(6);
+    /// assert_eq!(zero.to_string(Radix::Bin, precision), "0");
+    /// assert_eq!(zero.to_string(Radix::Oct, precision), "0");
+    /// assert_eq!(zero.to_string(Radix::Dec, precision), "0");
+    /// assert_eq!(zero.to_string(Radix::Hex, precision), "0");
     ///
-    /// assert_eq!(zero, "0");
-    /// assert_eq!(neg, "-0.1");
-    /// assert!(pi.starts_with("3.14159"));
-    /// ```
-    pub fn to_string_binary(&self, precision: u8) -> String {
-        todo!()
-    }
-
-    /// Get the octal string
+    /// assert_eq!(pi.to_string(Default::default(), precision), "3.141592");
+    /// assert_eq!(pi.to_string(Radix::Bin, precision), "11.001001");
+    /// assert_eq!(pi.to_string(Radix::Oct, precision), "3.110375");
+    /// assert_eq!(pi.to_string(Radix::Hex, precision), "3.243F6A");
     ///
+    /// assert_eq!(neg.to_string(Default::default(), precision), "-0.1");
+    /// assert_eq!(neg.to_string(Radix::Bin, precision), "-0.00011");
+    /// assert_eq!(neg.to_string(Radix::Oct, precision), "-0.063146");
+    /// assert_eq!(neg.to_string(Radix::Hex, precision), "-0.199999");
     /// ```
-    /// let pi = Number::PI.to_string_octal(6);
-    /// let zero = Number::ZERO.to_string_octal(6);
-    /// let neg = Number::from(-0.1).to_string_octal(6);
-    ///
-    /// assert_eq!(zero, "0");
-    /// assert_eq!(neg, "-0.063146");
-    /// assert!(pi, "3.11037");
-    /// ```
-    pub fn to_string_octal(&self, precision: u8) -> String {
-        todo!()
-    }
-
-    /// Get the hexadecimal string
-    ///
-    /// ```
-    /// let pi = Number::PI.to_string_hex(6);
-    /// let zero = Number::ZERO.to_string_hex(6);
-    /// let neg = Number::from(-0.1).to_string_hex(6);
-    ///
-    /// assert_eq!(zero, "0");
-    /// assert_eq!(neg, "-0.199999");
-    /// assert!(pi.starts_with("3.243F6A"));
-    /// ```
-    pub fn to_string_hex(&self, precision: u8) -> String {
+    pub fn to_string(&self, radix: Radix, precision: u8) -> String {
         todo!()
     }
 }
