e254910f2fb6f24d5096165c6a6bfc45ca372cee
Le Duy Nguyen <xnguye27@stud.fit.vut.cz>
2023-04-01 16:01:34 +0200

    Add basic arithmetic operations to `Number`

--- src/math/src/number.rs
+++ src/math/src/number.rs
@@ -1,9 +1,9 @@
+use crate::Result;
 use std::cmp::Ordering;
 
 /// Represent a number
 pub struct Number {
     inner: f64,
-    precision: u8,
 }
 
 impl AsRef<f64> for Number {
@@ -11,82 +11,62 @@ impl AsRef<f64> for Number {
         &self.inner
     }
 }
-
 impl Number {
     /// 0.0
-    pub const ZERO: Self = Self {
-        inner: 0.0,
-        precision: 6,
-    };
+    pub const ZERO: Self = Self { inner: 0.0 };
 
     /// 3.14159...
     pub const PI: Self = Self {
         inner: std::f64::consts::PI,
-        precision: 6,
     };
 
     /// 2.71828...
     pub const E: Self = Self {
         inner: std::f64::consts::E,
-        precision: 6,
     };
-
-    /// Set the maximum precision of the Number
-    /// This only affect the `to_string` (and its related) function
-    /// The calculation is unaffected
-    ///
-    /// ```
-    /// let mut e = Number::E;
-    /// assert_eq!(e.to_string(), "2.718281");
-    /// e.set_precision(2);
-    /// assert_eq!(e.to_string(), "2.71");
-    /// ```
-    pub fn set_precision(&mut self, precision: u8) {
-        self.precision = precision;
-    }
 
     /// Get the decimal string
     ///
     /// ```
-    /// let pi = Number::PI.to_string();
-    /// let zero = Number::ZERO.to_string();
-    /// let neg = Number::from(-0.1).to_string();
+    /// let pi = Number::PI.to_string(6);
+    /// let zero = Number::ZERO.to_string(6);
+    /// let neg = Number::from(-0.1).to_string(6);
     ///
     /// assert_eq!(zero, "0");
     /// assert_eq!(neg, "-0.1");
     /// assert!(pi, "3.141592"); // default precision is 6 decimal points
     /// ```
-    pub fn to_string(&self) -> String {
+    pub fn to_string(&self, precision: u8) -> String {
         todo!()
     }
 
     /// Get the binary string
     ///
     /// ```
-    /// let pi = Number::PI.to_string_binary();
-    /// let zero = Number::ZERO.to_string_binary();
-    /// let neg = Number::from(-0.1).to_string_binary();
+    /// let pi = Number::PI.to_string_binary(6);
+    /// let zero = Number::ZERO.to_string_binary(6);
+    /// let neg = Number::from(-0.1).to_string_binary(6);
     ///
     /// assert_eq!(zero, "0");
     /// assert_eq!(neg, "-0.1");
     /// assert!(pi.starts_with("3.14159"));
     /// ```
-    pub fn to_string_binary(&self) -> String {
+    pub fn to_string_binary(&self, precision: u8) -> String {
         todo!()
     }
 
     /// Get the octal string
     ///
     /// ```
-    /// let pi = Number::PI.to_string_octal();
-    /// let zero = Number::ZERO.to_string_octal();
-    /// let neg = Number::from(-0.1).to_string_octal();
+    /// let pi = Number::PI.to_string_octal(6);
+    /// let zero = Number::ZERO.to_string_octal(6);
+    /// let neg = Number::from(-0.1).to_string_octal(6);
     ///
     /// assert_eq!(zero, "0");
-    /// assert_eq!(neg, "-0.00011");
-    /// assert!(pi, "11.001001");
+    /// assert_eq!(neg, "-0.063146");
+    /// assert!(pi, "3.11037");
     /// ```
-    pub fn to_string_octal(&self) -> String {
+    pub fn to_string_octal(&self, precision: u8) -> String {
         todo!()
     }
 
@@ -101,7 +81,68 @@ impl Number {
     /// assert_eq!(neg, "-0.199999");
     /// assert!(pi.starts_with("3.243F6A"));
     /// ```
-    pub fn to_string_hex(&self) -> String {
+    pub fn to_string_hex(&self, precision: u8) -> String {
+        todo!()
+    }
+}
+
+impl Number {
+    /// Add two numbers together
+    ///
+    /// ```
+    /// let a = Number::from(0.1);
+    /// let b = Number::from(0.2);
+    ///
+    /// assert_eq!(a.add(b), Ok(Number::from(0.3)));
+    /// assert_eq!(b.add(a), a.add(b));
+    /// ```
+    pub fn add(&self, other: impl Into<Self>) -> Result<Self> {
+        todo!()
+    }
+
+    /// Subtract two numbers
+    ///
+    /// ```
+    /// let a = Number::from(0.1);
+    /// let b = Number::from(0.2);
+    ///
+    /// assert_eq!(a.sub(b), Ok(Number::from(-0.1)));
+    /// assert_ne!(b.sub(a), Ok(Number::from(0.1)));
+    /// ```
+    pub fn sub(&self, other: impl Into<Self>) -> Result<Self> {
+        todo!()
+    }
+
+    /// Multiply two numbers
+    ///
+    /// ```
+    /// let a = Number::from(0.1);
+    /// let b = Number::from(0.2);
+    ///
+    /// assert_eq!(a.mul(b), Ok(Number::from(-0.1)));
+    /// assert_ne!(b.mul(a), Ok(Number::from(0.1)));
+    /// ```
+    pub fn mul(&self, other: impl Into<Self>) -> Result<Self> {
+        todo!()
+    }
+
+    /// Divide two numbers
+    ///
+    /// # Error
+    /// Return Error::DivisionZero if `other` is 0
+    ///
+    /// ```
+    /// let random = Number::random();
+    /// assert!(random.div(Number::ZERO).is_err());
+    /// assert_eq!(Number::ZERO.div(random), Number::ZERO);
+    /// assert_eq!(rand.div(rand), Number::ONE);
+    /// let a = Number::from(3);
+    /// let b = Number::from(1.5);
+    /// let neg_b = Number::from(-1.5);
+    /// assert_eq!(a.div(b), Ok(Number::from(2)));
+    /// assert_eq!(a.div(neg_b), Ok(Number::from(-2)))
+    /// ```
+    pub fn div(&self, other: impl Into<Self>) -> Result<Self> {
         todo!()
     }
 }
