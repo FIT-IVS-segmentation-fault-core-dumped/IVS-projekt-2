002f9ea1bfeb822ba1b8a0459b9eb73e120f4198
Jakub Kloub <xkloub03@stud.fit.vut.cz>
2023-04-06 18:18:26 +0200

    Integrate localization

--- src/calc/src/main.rs
+++ src/calc/src/main.rs
@@ -1,11 +1,15 @@
 use calc::CalcState;
 use druid::{
-    widget::Flex, AppLauncher, Env, LocalizedString, Menu, MenuItem, Size, Widget, WindowDesc,
+    widget::Flex, AppLauncher, Env, Menu, MenuItem, Size, Widget, WindowDesc,
     WindowId,
 };
+use rust_i18n::t;
 
 const WINDOW_SIZE: Size = Size::new(400.0, 400.0);
 const MIN_WINDOW_SIZE: Size = Size::new(400.0, 400.0);
+
+// Initialize locales in "locales" directory.
+rust_i18n::i18n!("locales");
 
 fn build_root_widget() -> impl Widget<CalcState> {
     Flex::column()
@@ -13,19 +17,24 @@ fn build_root_widget() -> impl Widget<CalcState> {
 
 fn make_menu(_window: Option<WindowId>, _data: &CalcState, _env: &Env) -> Menu<CalcState> {
     Menu::empty()
-        .entry(MenuItem::new(LocalizedString::new("File")))
-        .entry(MenuItem::new(LocalizedString::new("Edit")))
-        .entry(MenuItem::new(LocalizedString::new("Help")))
+        .entry(MenuItem::new(t!("menu.file")))
+        .entry(MenuItem::new(t!("menu.edit")))
+        .entry(MenuItem::new(t!("menu.help")))
 }
 
 fn main() {
+    // Load stored calc state.
+    let calc_state = CalcState::new(available_locales());
+    // Set initial locale from config.
+    rust_i18n::set_locale(&calc_state.config.language);
+
     let main_window = WindowDesc::new(build_root_widget())
-        .title(LocalizedString::new("Title"))
+        .title(t!("title"))
         .window_size(WINDOW_SIZE)
         .with_min_size(MIN_WINDOW_SIZE)
         .menu(|window, data, env| make_menu(window, data, env));
 
     AppLauncher::with_window(main_window)
-        .launch(CalcState::new())
+        .launch(calc_state)
         .expect("Failed to launch application");
 }
