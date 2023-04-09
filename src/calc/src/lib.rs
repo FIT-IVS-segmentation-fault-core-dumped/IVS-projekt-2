3ffc56b0fc4ac304afaf1f523f9a291109f2744b
Jakub Kloub <xkloub03@stud.fit.vut.cz>
2023-04-09 22:38:45 +0200

    Add comments and light refractor

--- src/calc/src/lib.rs
+++ src/calc/src/lib.rs
@@ -1,11 +1,16 @@
-use std::rc::Rc;
+//! Calculator application
+//! 
+//! This file contains definition of calculator state,
+//! which defines functionality of our app.
 
+use std::rc::Rc;
 use druid::{Data, Lens};
 use math::number::Radix;
 use serde::{Deserialize, Serialize};
 
 const APP_NAME: &str = "Calculator";
 
+/// Operations on the calculator.
 #[rustfmt::skip]
 pub enum Opt {
     Sum, Sub, Mul, Div,
@@ -15,15 +20,29 @@ pub enum Opt {
     Abs, Comb, Fact
 }
 
+/// Used to map button presses to functionality.
+/// Now if we want to implement alternative ways
+/// of using our calculator, we just need to 
+/// pass this enum as action to the pressed button.
 pub enum PressedButton {
+    /// Numpad 0-9 or A-F (10 - 15)
     Num(u8),
     BinOpt(Opt),
     UnaryOpt(Opt),
+    /// Constant with given name.
+    /// Name is an entry to the table of constants, which will
+    /// contain its value. This gives use the ability, to define
+    /// their own constants (variables).
     Const(String),
+    /// Clear the entire display.
     Clear,
+    /// Delete only the item before cursor.
     Delete,
+    /// Move cursor right.
     MoveRight,
+    /// Move cursor left.
     MoveLeft,
+    /// Compute and display result.
     Evaluate,
     BracketLeft,
     BracketRight,
@@ -31,6 +50,7 @@ pub enum PressedButton {
     Ans,
 }
 
+/// Color theme of the app.
 #[derive(Serialize, Deserialize, Debug, Copy, Clone, Data, PartialEq)]
 pub enum Theme {
     Dark,
@@ -38,12 +58,15 @@ pub enum Theme {
     System,
 }
 
+/// Holds application configuration, which is saved on the disk.
+/// This is loaded at each start of the application.
 #[derive(Serialize, Deserialize, Lens, Clone, Data)]
 pub struct CalcConfig {
     theme: Theme,
-    pub language: String,
+    language: String,
 }
 
+/// Defines what will the initial config hold (when no config is found on the disk).
 impl Default for CalcConfig {
     fn default() -> Self {
         Self {
@@ -53,52 +76,76 @@ impl Default for CalcConfig {
     }
 }
 
+/// Holds shared data for all the widgets in our application.
+/// Each widget has access to instance of this struct.
 #[derive(Clone, Data, Lens)]
 pub struct CalcState {
+    /// Holds text, which is used to display the final string on the display widget.
     displayed_text: String,
+    /// Displayed base of the computed result.
     radix: Rc<Radix>,
+    /// Contains all available languages at runtime.
+    /// This is loaded from rust-i18n and as such has to be constructed in new() method.
     available_languages: Rc<Vec<String>>,
-    pub config: CalcConfig,
+    /// Confing deserialized from disk using *confy* crate.
+    config: CalcConfig,
 }
 
 impl CalcState {
-    pub fn new(languages: &'static [&'static str]) -> Self {
+    /// Creates new instance of CalcState. This will load config from disk.
+    /// Params:
+    /// - **languages**: Array of available languages loaded from rust-i18n.
+    pub fn new(languages: &[&str]) -> Self {
         let config = confy::load(APP_NAME, None).unwrap_or_default();
 
         Self {
             displayed_text: String::new(),
             radix: Rc::new(Radix::Dec),
+            // Convert array of string slices to vector of strings.
             available_languages: Rc::new(languages.iter().map(|&s| String::from(s)).collect()),
             config,
         }
     }
 
+    /// Readonly getter for language.
+    pub fn language(&self) -> &String {
+        &self.config.language
+    }
+
+    /// Handle button event. We will construct the displayed string using this method.
     pub fn process_button(&self, _button: PressedButton) {
         todo!();
     }
 
+    /// Convert CalcState::display_string to *evaluate string*, which 
+    /// can then be passed to the `math::evaluate` function.
     pub fn get_eval_str(&self) -> String {
         todo!()
     }
 
-    pub fn store_config_data(&self) {
-        confy::store(APP_NAME, None, &self.config).unwrap();
+    /// Store CalcState::config on the disk using *confy* create.
+    pub fn store_config_data(&self) -> Result<(), confy::ConfyError> {
+        confy::store(APP_NAME, None, &self.config)
     }
 
+    /// Get currently active theme.
     pub fn get_theme(&self) -> Theme {
         self.config.theme
     }
 
+    /// Change theme of application. This will be saved at exit.
     pub fn set_theme(&mut self, theme: Theme) {
         self.config.theme = theme;
     }
 
+    /// Change language of the app. This will be saved at exit.
     pub fn set_language(&mut self, language: &str) {
         assert!(self.available_languages.contains(&language.to_string()));
         rust_i18n::set_locale(language);
         self.config.language = String::from(language);
     }
 
+    /// Change numeric base of the calculated results.
     pub fn set_radix(&mut self, radix: Radix) {
         self.radix = Rc::new(radix);
     }
--- src/calc/src/main.rs
+++ src/calc/src/main.rs
@@ -5,16 +5,19 @@ use druid::{
 };
 use rust_i18n::t;
 
+/// Initial size of the window, when the app starts.
 const WINDOW_SIZE: Size = Size::new(400.0, 400.0);
 const MIN_WINDOW_SIZE: Size = Size::new(400.0, 400.0);
 
 // Initialize locales in "locales" directory.
 rust_i18n::i18n!("locales");
 
+/// Creates the root widget of app. All other widgets are inside this one.
 fn build_root_widget() -> impl Widget<CalcState> {
     Flex::column()
 }
 
+/// Creates menu on top of the main window.
 fn make_menu(_window: Option<WindowId>, _data: &CalcState, _env: &Env) -> Menu<CalcState> {
     Menu::empty()
         .entry(MenuItem::new(t!("menu.file")))
@@ -26,15 +29,19 @@ fn main() {
     // Load stored calc state.
     let calc_state = CalcState::new(available_locales());
     // Set initial locale from config.
-    rust_i18n::set_locale(&calc_state.config.language);
+    rust_i18n::set_locale(calc_state.language());
 
+    // Create the main window with given window parameters.
     let main_window = WindowDesc::new(build_root_widget())
         .title(t!("title"))
         .window_size(WINDOW_SIZE)
         .with_min_size(MIN_WINDOW_SIZE)
         .menu(|window, data, env| make_menu(window, data, env));
 
-    AppLauncher::with_window(main_window)
-        .launch(calc_state)
-        .expect("Failed to launch application");
+    // Launch the main app using calc_state to define behaviour.
+    if let Err(platform_err) = AppLauncher::with_window(main_window)
+        .launch(calc_state) {
+            eprintln!("error: Failed to launch main application. {}", platform_err);
+            std::process::exit(1);
+    }
 }
