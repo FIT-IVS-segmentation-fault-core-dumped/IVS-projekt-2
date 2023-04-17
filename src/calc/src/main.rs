2868b16b2fa1e4920f6d7bffa9821fb220bbfe07
Matúš Moravčík <xmorav48@vutbr.cz>
2023-04-17 18:19:51 +0200

    Refactoring `main.rs`

--- src/calc/src/main.rs
+++ src/calc/src/main.rs
@@ -1,30 +1,42 @@
-use calc::CalcState;
-use druid::{widget::Flex, AppLauncher, Env, Menu, MenuItem, Size, Widget, WindowDesc, WindowId};
+mod environment;
+
+use calc::translate;
+use calc::widgets::menu::CalcMenu;
+use calc::{widgets::ButtonsUI, CalcState, Theme};
+use druid::{
+    theme,
+    widget::{Container, EnvScope, Flex},
+    AppLauncher, Size, Widget, WindowDesc,
+};
+use environment::*;
 use rust_i18n::t;
 
 /// Initial size of the window, when the app starts.
 const WINDOW_SIZE: Size = Size::new(400.0, 400.0);
 const MIN_WINDOW_SIZE: Size = Size::new(400.0, 400.0);
 
-// Initialize locales in "locales" directory.
-rust_i18n::i18n!("locales");
-
 /// Creates the root widget of app. All other widgets are inside this one.
 fn build_root_widget() -> impl Widget<CalcState> {
-    Flex::column()
-}
-
-/// Creates menu on top of the main window.
-fn make_menu(_window: Option<WindowId>, _data: &CalcState, _env: &Env) -> Menu<CalcState> {
-    Menu::empty()
-        .entry(MenuItem::new(t!("menu.file")))
-        .entry(MenuItem::new(t!("menu.edit")))
-        .entry(MenuItem::new(t!("menu.help")))
+    EnvScope::new(
+        |env, data| match data.get_theme(true) {
+            Theme::Dark => set_dark_envs(env),
+            Theme::Light => set_light_envs(env),
+            Theme::System => unreachable!(),
+        },
+        Container::new(
+            Flex::column()
+                .main_axis_alignment(druid::widget::MainAxisAlignment::End)
+                .with_flex_child(Flex::column(), 1.0)
+                // .with_spacer(10.0)
+                .with_flex_child(ButtonsUI::build_ui(), 3.0),
+        )
+        .background(theme::WINDOW_BACKGROUND_COLOR),
+    )
 }
 
 fn main() {
     // Load stored calc state.
-    let calc_state = CalcState::new(available_locales());
+    let calc_state = CalcState::new(calc::available_locales());
     // Set initial locale from config.
     rust_i18n::set_locale(calc_state.language());
 
@@ -33,10 +45,23 @@ fn main() {
         .title(t!("title"))
         .window_size(WINDOW_SIZE)
         .with_min_size(MIN_WINDOW_SIZE)
-        .menu(make_menu);
+        .menu(CalcMenu::build_ui);
 
     // Launch the main app using calc_state to define behaviour.
-    if let Err(platform_err) = AppLauncher::with_window(main_window).launch(calc_state) {
+    if let Err(platform_err) = AppLauncher::with_window(main_window)
+        .configure_env(|env, data| {
+            if data.get_theme(true) == Theme::Dark {
+                set_dark_envs(env);
+            } else {
+                set_light_envs(env);
+            };
+
+            set_digit_btn_envs(env);
+            set_func_btn_envs(env);
+            set_operation_btn_envs(env);
+        })
+        .launch(calc_state)
+    {
         eprintln!("error: Failed to launch main application. {}", platform_err);
         std::process::exit(1);
     }
