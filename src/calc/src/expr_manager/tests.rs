a30bf1b82fcaf5cae7df94507c0cc88d013707c0
Jakub Kloub <xkloub03@stud.fit.vut.cz>
2023-04-21 20:33:41 +0200

    Fix `expr_manager::tests`

--- src/calc/src/expr_manager/tests.rs
+++ src/calc/src/expr_manager/tests.rs
@@ -8,7 +8,7 @@ use super::{ExprManager, Btn, ToExpr};
 fn convert(btn_stack: Vec<Btn>) -> String {
     let mut expr_man = ExprManager::new();
     expr_man.btn_stack = btn_stack;
-    expr_man.get_eval_str()
+    expr_man.get_eval_str().unwrap()
 }
 
 // Convert given string to sequence of `PressedButton` enums.
@@ -32,7 +32,7 @@ fn to_opt_seq(str: &str, rep: Vec<&Opt>) -> Vec<Btn> {
         }
 
         let num = c.to_digit(10).unwrap_or(10) as u8;
-        if (0..9).contains(&num) {
+        if (0..10).contains(&num) {
             btns.push(Btn::Num(num));
             continue;
         }
@@ -42,13 +42,17 @@ fn to_opt_seq(str: &str, rep: Vec<&Opt>) -> Vec<Btn> {
             '-' => Btn::BinOpt(Opt::Sub),
             '*' => Btn::BinOpt(Opt::Mul),
             '/' => Btn::BinOpt(Opt::Div),
-            '!' => Btn::BinOpt(Opt::Fact),
+            '!' => Btn::UnaryOpt(Opt::Fact),
             '(' => Btn::BracketLeft,
             ')' => Btn::BracketRight,
             '.' => Btn::Comma,
             '@' => {
                 n_rep += 1;
                 Btn::BinOpt((*rep.get(n_rep - 1).unwrap()).to_owned())
+            },
+            '&' => {
+                n_rep += 1;
+                Btn::UnaryOpt((*rep.get(n_rep - 1).unwrap()).to_owned())
             },
             '$' => {
                 expect_const = true;
@@ -91,13 +95,13 @@ fn bin_opt_template(display_opt: &Opt, eval_opt: &Opt) {
 
     // convert("3root8*4root16") == "root(3,8)*root(4,16)"
     assert_eq!(
-        convert(to_opt_seq("3@8*4@16", Vec::from([display_opt]))),
+        convert(to_opt_seq("3@8*4@16", Vec::from([display_opt, display_opt]))),
         format!("{}(3,8)*{}(4,16)", eval, eval)
     );
 
     // convert("3root(4root16)") == "root(3,root(4,16))"
     assert_eq!(
-        convert(to_opt_seq("3@(4@16)", Vec::from([display_opt]))),
+        convert(to_opt_seq("3@(4@16)", Vec::from([display_opt, display_opt]))),
         format!("{}(3,{}(4,16))", eval, eval)
     );
 }
@@ -107,43 +111,43 @@ fn unary_opt_template(display_opt: &Opt, eval_opt: &Opt) {
     let eval = eval_opt.to_expr().unwrap().eval;
     // convert("sqrt8") == "sqrt(8)"
     assert_eq!(
-        convert(to_opt_seq("@8", Vec::from([display_opt]))),
+        convert(to_opt_seq("&8", Vec::from([display_opt]))),
         format!("{}(8)", eval)
     );
 
     // convert("sqrt16+2") == "sqrt(16)+2"
     assert_eq!(
-        convert(to_opt_seq("@16+2", Vec::from([display_opt]))),
+        convert(to_opt_seq("&16+2", Vec::from([display_opt]))),
         format!("{}(16)+2", eval)
     );
 
     // convert("4+8sqrt6") == "4+8sqrt(6)"
     assert_eq!(
-        convert(to_opt_seq("4+8@6", Vec::from([display_opt]))),
-        format!("4+8{}(6)", eval)
+        convert(to_opt_seq("4+8&6", Vec::from([display_opt]))),
+        format!("4+8*{}(6)", eval)
     );
 
     // convert("7/21sqrt(15)") == "7/21sqrt(15)"
     assert_eq!(
-        convert(to_opt_seq("7/21@(15)", Vec::from([display_opt]))),
-        format!("7/21{}(15)", eval)
+        convert(to_opt_seq("7/21&(15)", Vec::from([display_opt]))),
+        format!("7/21*{}(15)", eval)
     );
 
     // convert("sqrt4*sqrt9") == "sqrt(4)*sqrt(9)"
     assert_eq!(
-        convert(to_opt_seq("@4*@9", Vec::from([display_opt]))),
+        convert(to_opt_seq("&4*&9", Vec::from([display_opt, display_opt]))),
         format!("{}(4)*{}(9)", eval, eval)
     );
 
     // convert("sqrt4sqrt9") == "sqrt(4)sqrt(9)"
     assert_eq!(
-        convert(to_opt_seq("@4@9", Vec::from([display_opt]))),
-        format!("{}(4){}(9)", eval, eval)
+        convert(to_opt_seq("&4&9", Vec::from([display_opt, display_opt]))),
+        format!("{}(4)*{}(9)", eval, eval)
     );
 
     // convert("sqrt(sqrt16)") == "sqrt(sqrt(16))"
     assert_eq!(
-        convert(to_opt_seq("@(@16)", Vec::from([display_opt]))),
+        convert(to_opt_seq("&(&16)", Vec::from([display_opt, display_opt]))),
         format!("{}({}(16))", eval, eval)
     );
 }
@@ -167,7 +171,7 @@ fn convert_sub() {
     assert_eq!(convert(to_opt_seq("-0.1-1.2", Vec::new())), "-0.1-1.2");
     assert_eq!(convert(to_opt_seq("4--8", Vec::new())), "4--8");
     assert_eq!(convert(to_opt_seq("4--8-2", Vec::new())), "4--8-2");
-    assert_eq!(convert(to_opt_seq("-(-1-(2))", Vec::new())), "-(-1-(2))");
+    assert_eq!(convert(to_opt_seq("-(-1-(2))", Vec::new())), "-(-1-2)");
 }
 
 #[test]
@@ -191,7 +195,7 @@ fn convert_fact() {
     assert_eq!(convert(to_opt_seq("(-2*3)!", Vec::new())), "(-2*3)!");
     assert_eq!(convert(to_opt_seq("4+16!*4", Vec::new())), "4+16!*4");
     assert_eq!(convert(to_opt_seq("4!*9!", Vec::new())), "4!*9!");
-    assert_eq!(convert(to_opt_seq("4!9!", Vec::new())), "4!9!");
+    assert_eq!(convert(to_opt_seq("4!9!", Vec::new())), "4!*9!");
 }
 
 #[test]
@@ -211,12 +215,12 @@ fn convert_e() {
 #[test]
 fn convert_expression() {
     assert_eq!(
-        convert(to_opt_seq("@4@+2@(@0.707)", Vec::from([&Opt::Sqrt, &Opt::Fact, &Opt::Root, &Opt::Cos]))),
-        "sqrt(fact(4))+root(2,cos(0.707))"
+        convert(to_opt_seq("&4&+2@(&0.707)", Vec::from([&Opt::Sqrt, &Opt::Fact, &Opt::Root, &Opt::Cos]))),
+        "sqrt(4!)+root(2,cos(0.707))"
     );
     assert_eq!( 
         convert(to_opt_seq("2@($e$+5@3)", Vec::from([&Opt::Pow, &Opt::Comb]))),
-        "pow(2,e()+comb(5,3))"
+        "2^(e()+comb(5,3))"
     );
 }
 
@@ -228,11 +232,6 @@ fn convert_root() {
 #[test]
 fn convert_sqrt() {
     unary_opt_template(&Opt::Sqrt, &Opt::Sqrt)
-}
-
-#[test]
-fn convert_pow() {
-    bin_opt_template(&Opt::Pow, &Opt::Pow)
 }
 
 #[test]
