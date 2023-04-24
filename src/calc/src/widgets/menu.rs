e02ed053c98fc7b0d859785ef2c4627efa317e84
Matúš Moravčík <xmorav48@vutbr.cz>
2023-04-24 22:48:16 +0200

    Add about, history, angular unit and copy tabs + update localization

--- src/calc/src/widgets/menu.rs
+++ src/calc/src/widgets/menu.rs
@@ -2,12 +2,16 @@
 
 use std::rc::Rc;
 
-use druid::{Env, Menu, MenuItem, WindowId};
+use druid::{Command, Env, Menu, MenuItem, Selector, Target, WindowId};
 use rust_i18n::*;
 
 use crate::{CalcState, Theme};
 
 pub struct CalcMenu;
+
+const SHOW_HISTORY: Selector<String> = Selector::new("show_history");
+const SHOW_HELP: Selector<String> = Selector::new("show_help");
+const SHOW_ABOUT: Selector<String> = Selector::new("show_about");
 
 impl CalcMenu {
     pub fn build_ui(_window: Option<WindowId>, _data: &CalcState, _env: &Env) -> Menu<CalcState> {
@@ -22,24 +26,72 @@ fn make_menu(_window: Option<WindowId>, _data: &CalcState, _env: &Env) -> Menu<C
             old_data.get_language() != data.get_language()
         })
         .entry(
-            Menu::new(t!("menu.file"))
+            Menu::new(t!("menu.options"))
                 .entry(
-                    Menu::new(t!("theme"))
+                    Menu::new(t!("options.theme"))
                         .entry(make_theme_button(Theme::Dark))
                         .entry(make_theme_button(Theme::Light))
                         .entry(make_theme_button(Theme::System)),
                 )
                 .entry(
-                    Menu::new(t!("language"))
+                    Menu::new(t!("options.language"))
                         .entry(make_language_button("cz".to_owned()))
                         .entry(make_language_button("en".to_owned()))
                         .entry(make_language_button("jp".to_owned()))
                         .entry(make_language_button("sk".to_owned()))
                         .entry(make_language_button("vi".to_owned())),
-                ),
+                )
+                .entry(
+                    Menu::new(t!("options.angular_unit"))
+                        .entry(make_angular_unit_button("radians", false))
+                        .entry(make_angular_unit_button("degrees", true)),
+                )
+                .entry(
+                    MenuItem::new(t!("options.record_hist"))
+                        .on_activate(|_ctx, data: &mut CalcState, _env| {
+                            data.config.history.toggle_recording();
+                            data.store_config_data();
+                        })
+                        .selected_if(|data, _env| data.get_history().recording() == true),
+                )
+                .entry(MenuItem::new(t!("options.show_hist")).command(Command::new(
+                    SHOW_HISTORY,
+                    "".to_owned(),
+                    Target::Auto,
+                ))),
         )
-        .entry(MenuItem::new(t!("menu.edit")))
-        .entry(MenuItem::new(t!("menu.help")))
+        .entry(
+            Menu::new(t!("menu.edit"))
+                .entry(MenuItem::new(t!("edit.copy_res")).on_activate(
+                    |_ctx, data: &mut CalcState, _env| {
+                        data.copy_result();
+                    },
+                ))
+                .entry(MenuItem::new(t!("edit.copy_expr")).on_activate(
+                    |_ctx, data: &mut CalcState, _env| {
+                        data.copy_expression();
+                    },
+                )),
+        )
+        .entry(MenuItem::new(t!("menu.help")).command(Command::new(
+            SHOW_HELP,
+            "".to_owned(),
+            Target::Global,
+        )))
+        .entry(MenuItem::new(t!("menu.about")).command(Command::new(
+            SHOW_ABOUT,
+            "".to_owned(),
+            Target::Global,
+        )))
+}
+
+fn make_angular_unit_button(name: &str, is_degree: bool) -> MenuItem<CalcState> {
+    let text = format!("angular_units.{}", name);
+    MenuItem::new(t!(&text))
+        .on_activate(move |_ctx, data: &mut CalcState, _env| {
+            data.set_angular_unit(is_degree);
+        })
+        .selected_if(move |data, _env| data.get_angular_unit() == is_degree)
 }
 
 fn make_theme_button(theme: Theme) -> MenuItem<CalcState> {
