3a200b3e08c8a224f54c8169a9f2b2d8a675859a
Matúš Moravčík <xmorav48@vutbr.cz>
2023-04-25 23:45:19 +0200

    Update  `About` window

--- src/calc/src/delegate.rs
+++ src/calc/src/delegate.rs
@@ -19,7 +19,7 @@ const SHOW_ABOUT: Selector<String> = Selector::new("show_about");
 
 const HISTORY_WIN: Size = Size::new(300.0, 300.0);
 const HELP_WIN: Size = Size::new(400.0, 400.0);
-const ABOUT_WIN: Size = Size::new(300.0, 300.0);
+const ABOUT_WIN: Size = Size::new(500.0, 400.0);
 
 /// Handle the menu bar commands for opening windows
 pub struct Delegate;
@@ -40,6 +40,7 @@ impl AppDelegate<CalcState> for Delegate {
 
     #[rustfmt::skip]
     fn command(&mut self, ctx: &mut DelegateCtx, _target: Target, cmd: &Command,data: &mut CalcState,_env: &Env) -> Handled {
+
         // Display History window
         if let Some(_) = cmd.get(SHOW_HISTORY) {
             if !data.get_history().is_opened() {
